0f1f5b8a5058e3a423052fa00e9935c67f18f4bd
karenclotilde <karen.clotilde@gmail.com>
2019-05-20 17:16:58 +0200

    user service fait sur le modèle de authentification service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from './model/user';
+import {Observable} from 'rxjs';
+import {CanActivate, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService implements CanActivate {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {
+
+  }
+
+  private _isLogged = false;
+
+
+  get isLogged(): boolean {
+    return this._isLogged;
+  }
+
+  public authentication(user: User): Observable<any> {
+    const headers: HttpHeaders = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa(`${user.email}:${user.motDePasse}`)
+      }
+    );
+    return this.http.get('http://localhost:8080/la-factory/rest/login', {headers: headers});
+
+  }
+
+  public login(user: User) {
+    sessionStorage.setItem('isLoggedIn', 'true');
+    sessionStorage.setItem('login', user.email);
+    this._isLogged = true;
+  }
+
+  public logout() {
+    sessionStorage.clear();
+    this._isLogged = false;
+  }
+
+  verifLog() {
+    if (this.isLogged) {
+      return true;
+    } else {
+      this.router.navigate(['/login', 'error']);
+      return false;
+    }
+  }
+
+  canActivate() {
+
+    return this.verifLog();
+  }
 }
