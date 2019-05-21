96694b1417a61bb6cc4f717321045dd53ba35310
Sylvain Chea <sylvainmb@hotmail.com>
2019-05-21 11:06:01 +0200

    matiere route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {UserService} from "./user.service";
+import {MatieresComponent} from './matieres/matieres.component';
 
 
 export const routes: Routes = [{path: 'login', component: LoginComponent},
   {path: 'login/:error', component: LoginComponent},
+  { path : 'matiere/edit', component: MatieresComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}];
