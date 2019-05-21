5fef9c4ebcfbdda16753b5a2b313902f79cfcca2
helene <helene.yu@icd.igsgroupe.com>
2019-05-21 16:57:06 +0200

    routes

---
+++ src/app/matiere-edit/matiere-edit.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpHeaders} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Matiere} from '../model/matiere';
+import {MatieresService} from '../matieres.service';
+import {connectableObservableDescriptor} from 'rxjs/internal/observable/ConnectableObservable';
+
+@Component({
+  selector: 'app-matiere-edit',
+  templateUrl: './matiere-edit.component.html',
+  styleUrls: ['./matiere-edit.component.css']
+})
+export class MatiereEditComponent implements OnInit {
+
+  private matiere: Matiere = new Matiere();
+  private monForm: FormGroup;
+
+
+
+  private message = 'pseudo et/ou mot de passe incorrect(s)';
+
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router, private matiereService: MatieresService) {
+
+  }
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      if (params.error) {
+        this.message = 'il faut vous connecter pour aller sur cette page';
+      }});
+  }
+
+  send() {
+    this.matiereService.insert(this.matiere).subscribe( data => {
+      console.log('ok')}, error => { console.log(error);} );
+  }
+}
