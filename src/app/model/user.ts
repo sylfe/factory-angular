62c0a7600bdb941af12686b3ad8227d5cc6cf7f0
helene <helene.yu@icd.igsgroupe.com>
2019-05-21 10:36:39 +0200

    models

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,9 @@
+import {Droit} from './droit.enum';
+
 export class User {
+
+  constructor(private id?: number, private nom?: string, private prenom?: string, private adresse?: string,
+              private mail?: string, private telephone?: string, private motDePasse?: string,
+              private version?: number, private droit?: Droit) {}
+
 }
