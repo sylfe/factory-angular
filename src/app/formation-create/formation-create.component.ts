e9234d4e30633f6f893aee519fbb312850358f57
Alexis <drouinalexis@hotmail.fr>
2019-05-22 15:15:25 +0200

    Debut formation edit

--- src/app/formation-create/formation-create.component.ts
+++ src/app/formation-create/formation-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormateurService} from '../formateur.service';
+import {Formation} from '../model/formation';
 
 @Component({
   selector: 'app-formation-create',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormationCreateComponent implements OnInit {
 
-  constructor() { }
+  private formation: Formation = new Formation();
+  constructor(private activedRoute: ActivatedRoute,
+              private formationService: FormationService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.activedRoute.params.subscribe( params => {
+      if (params.id) {
+        this.formationService.findById(params.id).subscribe( data => {
+          this.formation = data;
+        });
+      }
+    });
+  }
+
+  save() {
+    if (this.formation.id) {
+      this.formationService.update(this.formation).subscribe(result => {
+        this.router.navigate(['/formation', 'modifié', this.formation.titre]);
+      });
+    } else {
+      console.log('ok');
+      this.formationService.insert(this.formation).subscribe( result => {
+        this.router.navigate(['/formateur', 'ajouté', this.formateur.nom]);
+      });
+    }
   }
 
 }
