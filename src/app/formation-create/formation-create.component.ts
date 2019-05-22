ae3f8a92974edce10b1b0edaf7fecf628096cb1a
Constance Royal <constance.royal@gmail.com>
2019-05-22 18:37:43 +0200

    formation suite

--- src/app/formation-create/formation-create.component.ts
+++ src/app/formation-create/formation-create.component.ts
@@ -12,23 +12,29 @@ export class FormationCreateComponent implements OnInit {
   private formation: Formation = new Formation();
 
 
-
   constructor(private activatedRoute: ActivatedRoute, private formationService: FormationService, private router: Router) {
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe( params => {
-      if(params.id) {
+    this.activatedRoute.params.subscribe(params => {
+      if (params.id) {
         this.formationService.findById(params.id).subscribe(data => {
           this.formation = data;
         });
       }
     });
   }
+
   save() {
-    this.formationService.insert(this.formation).subscribe(result => {
-      this.router.navigate(['/formation/edit', this.formation.id]);
-    });
+    if (this.formation.id) {
+      this.formationService.update(this.formation).subscribe(result => {
+        this.router.navigate(['/formation', 'edit', this.formation.id]);
+      });
+    } else {
+      this.formationService.insert(this.formation).subscribe(result => {
+        console.log(result);
+        this.router.navigate(['/formation', 'edit', result.id]);
+      });
+    }
   }
 }
-
