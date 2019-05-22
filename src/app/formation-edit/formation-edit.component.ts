61e33c96dd222db0f6c6331a15e407fa01b73c27
Alexis <drouinalexis@hotmail.fr>
2019-05-22 16:13:36 +0200

    formation edit

--- src/app/formation-edit/formation-edit.component.ts
+++ src/app/formation-edit/formation-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Formation} from '../model/formation';
+import {FormationService} from '../formation.service';
+import {Matiere} from '../model/matiere';
+import {MatieresService} from '../matieres.service';
+import {Formateur} from '../model/formateur';
+import {FormateurService} from '../formateur.service';
 
 @Component({
   selector: 'app-formation-edit',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormationEditComponent implements OnInit {
 
-  constructor() { }
+  private matieres: Matiere[];
+  private formateurs: Formateur[];
+
+  private formation: Formation = new Formation();
+
+  constructor(private activedRoute: ActivatedRoute,
+              private formationService: FormationService,
+              private matiereService: MatieresService,
+              private formateurServise: FormateurService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.activedRoute.params.subscribe( params => {
+      if (params.id) {
+        this.formationService.findById(params.id).subscribe( data => {
+          this.formation = data;
+        });
+      }
+    });
+    this.listMatiere();
   }
 
+  save() {
+    if (this.formation.id) {
+      this.formationService.update(this.formation).subscribe(result => {
+        this.router.navigate(['/formation', 'modifié', this.formation.titre]);
+      });
+    } else {
+      console.log('ok');
+      this.formationService.insert(this.formation).subscribe( result => {
+        this.router.navigate(['/formation', 'ajouté', this.formation.titre]);
+      });
+    }
+  }
+
+  listMatiere() {
+    this.matiereService.list().subscribe( data => {
+      this.matieres = data;
+      console.log(this.matieres);
+    }, error => {
+      console.log('error');
+    });
+  }
+}
 }
