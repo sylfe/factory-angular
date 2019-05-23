44631e53bc42e7efbcfae7d91f99ed2d8cc496d6
EmmanuelleTEK <emmanuelle.ttek@hotmail.fr>
2019-05-23 15:12:18 +0200

    salle edit à débugger

--- src/app/salle-edit/salle-edit.component.ts
+++ src/app/salle-edit/salle-edit.component.ts
@@ -20,6 +20,8 @@ export class SalleEditComponent implements OnInit {
 
   private salle: Salle = new Salle();
   private videoprojecteurs: VideoProjecteur[];
+  private salleAvecVideo: boolean;
+  private pasVideop: VideoProjecteur = new VideoProjecteur(null, null, null, null);
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
@@ -27,10 +29,11 @@ export class SalleEditComponent implements OnInit {
 
         this.salleService.findById(params.id).subscribe(data => {
           this.salle = data;
+          console.log(this.salle);
         });
       }
     });
-    this.videoprojecteurService.list().subscribe(result => {
+    this.videoprojecteurService.listLibre().subscribe(result => {
       this.videoprojecteurs = result;
     });
     this.salle.videoprojecteur = new VideoProjecteur();
@@ -48,6 +51,21 @@ export class SalleEditComponent implements OnInit {
       });
     }
   }
+
+
+  video(){
+    console.log('J\'ai cliqué sur : ' + this.salleAvecVideo);
+    if(this.salleAvecVideo === false){
+      this.salleAvecVideo = false;
+      console.log('clic sur false : ' + this.salle.videoprojecteur.id);
+      this.salle.videoprojecteur.id = 0;
+      console.log('après clic : ' + this.salle.videoprojecteur.id);
+    } else {
+      this.salleAvecVideo = true;
+      console.log('clic sur true : ' + this.salleAvecVideo);
+    }
+  }
+
 
 }
 
