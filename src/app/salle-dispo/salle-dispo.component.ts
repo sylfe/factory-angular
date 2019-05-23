2286344a39aeab38f0e7274c2ef43829c2e93169
Sylvain Chea <sylvainmb@hotmail.com>
2019-05-23 15:03:27 +0200

    salledispo

---
+++ src/app/salle-dispo/salle-dispo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salle-dispo',
+  templateUrl: './salle-dispo.component.html',
+  styleUrls: ['./salle-dispo.component.css']
+})
+export class SalleDispoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
